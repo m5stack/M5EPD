5e817e4706109da3f523ede402c8c3e24e623883
Sean <1026025056@qq.com>
2021-02-01 19:01:42 +0800

    replace RTC example

--- examples/Basics/RTC_BM8563/RTC_BM8563.ino
+++ examples/Basics/RTC_BM8563/RTC_BM8563.ino
@@ -2,31 +2,48 @@
 
 M5EPD_Canvas canvas(&M5.EPD);
 
-void setup()
-{
-    M5.begin();
-    M5.EPD.SetRotation(90);
-    M5.TP.SetRotation(90);
-    M5.EPD.Clear(true);
-    M5.RTC.begin();
-    canvas.createCanvas(540, 960);
-    canvas.setTextSize(3);
-    canvas.drawString("Press PWR Btn for sleep!", 45, 350);
-    canvas.drawString("after 5 sec wakeup!", 70, 450);
-    canvas.pushCanvas(0,0,UPDATE_MODE_DU4);
+rtc_time_t RTCtime;
+rtc_date_t RTCDate;
+
+char timeStrbuff[64];
+
+void flushTime(){
+    M5.RTC.getTime(&RTCtime);
+    M5.RTC.getDate(&RTCDate);
     
-    
+    sprintf(timeStrbuff,"%d/%02d/%02d %02d:%02d:%02d",
+                        RTCDate.year,RTCDate.mon,RTCDate.day,
+                        RTCtime.hour,RTCtime.min,RTCtime.sec);
+                                         
+    canvas.drawString(timeStrbuff, 0, 0);
+    canvas.pushCanvas(100,200,UPDATE_MODE_DU4);
 }
 
-void loop()
-{
-    if(M5.BtnP.wasPressed()){
-        
-        canvas.drawString("I'm going to sleep.zzzZZZ~", 45, 550);
-        canvas.pushCanvas(0,0,UPDATE_MODE_DU4);
-        delay(1000);
-        M5.shutdown(5);
-    }
-    M5.update();
-    delay(100);
+void setupTime(){
+  
+  RTCtime.hour = 23;
+  RTCtime.min = 33;
+  RTCtime.sec = 33;
+  M5.RTC.setTime(&RTCtime);
+  
+  RTCDate.year = 2020;
+  RTCDate.mon = 11;
+  RTCDate.day = 27;
+  M5.RTC.setDate(&RTCDate);
+}
+
+void setup() {
+
+    M5.begin();
+    M5.EPD.SetRotation(90);
+    M5.EPD.Clear(true);
+    M5.RTC.begin();
+    canvas.createCanvas(400, 300);
+    canvas.setTextSize(3);
+    setupTime();
+}
+
+void loop() {
+  flushTime();
+  delay(1000);
 }
