4d7c4f1ad82aa9380b0900b687b3b39b8300a1ed
estshorter <1430311+estshorter@users.noreply.github.com>
2021-02-03 19:07:55 +0900

    Massive refactoring for BM8563

--- src/utility/BM8563.h
+++ src/utility/BM8563.h
@@ -15,9 +15,9 @@ typedef struct RTC_Time
 typedef struct RTC_Date
 {
     int8_t week;
-    int8_t mon;
+    int8_t mon; // [1, 12]
     int8_t day;
-    int16_t year;
+    int16_t year; // AD
     RTC_Date() : week(), mon(), day(), year() {}
     RTC_Date(int8_t w, int8_t m, int8_t d, int16_t y) : week(w), mon(m), day(d), year(y) {}
 } rtc_date_t;
@@ -25,32 +25,58 @@ typedef struct RTC_Date
 class BM8563
 {
 public:
-    BM8563();
+    BM8563(TwoWire &wire = Wire) : _wire(wire){};
 
-    void begin(void);
+    void begin(int sda = -1, int scl = -1, uint32_t frequency = 0);
     void writeReg(uint8_t reg, uint8_t data);
     uint8_t readReg(uint8_t reg);
-    void getTime(void);
 
     void setTime(const rtc_time_t *time);
     void setDate(const rtc_date_t *date);
+    void setDateTime(const rtc_date_t *date, const rtc_time_t *time);
 
     void getTime(rtc_time_t *time);
     void getDate(rtc_date_t *date);
+    void getDateTime(rtc_date_t *date, rtc_time_t *time);
 
     int setAlarmIRQ(int afterSeconds);
     int setAlarmIRQ(const rtc_time_t &time);
     int setAlarmIRQ(const rtc_date_t &date, const rtc_time_t &time);
-    void clearIRQ();
-    void disableIRQ();
+    void clearIRQ(void);
+    void disableIRQ(void);
 
 private:
-    void DataMask();
+    TwoWire &_wire;
     uint8_t Bcd2ToByte(uint8_t Value);
     uint8_t ByteToBcd2(uint8_t Value);
+    void readDate(rtc_date_t *date, uint8_t *buf);
+    void readTime(rtc_time_t *time, uint8_t *buf);
+    void writeDate(const rtc_date_t *date);
+    void writeTime(const rtc_time_t *time);
 
-private:
-    uint8_t trdata[7];
+    static constexpr int Addr = 0x51;
+    enum Register
+    {
+        ControlStatus1 = 0x00,
+        ControlStatus2 = 0x01,
+        Second = 0x02,
+        Day = 0x05,
+        MinuteAlarm = 0x09,
+        TimerControl = 0x0E,
+        Timer = 0x0F,
+        ClkoutControl = 0x0D
+    };
+
+    struct Mask
+    {
+        static constexpr int Second = 0x7f;
+        static constexpr int Minute = 0x7f;
+        static constexpr int Hour = 0x3f;
+        static constexpr int Day = 0x3f;
+        static constexpr int Week = 0x07;
+        static constexpr int Month = 0x1f;
+        static constexpr int Year = 0xff;
+    };
 };
 
 #endif
